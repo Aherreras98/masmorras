900f460dbfe980145e28cc3e035b36d0ddea8e6d
Adrián  Herrera <aherrerasosa98@gmail.com>
2025-05-11 16:13:27 +0200

    Vista creacion personaje

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.adrijavi {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.adrijavi to javafx.fxml;
+    opens com.adrijavi to javafx.graphics;
+    opens com.adrijavi.controlador to javafx.fxml;
     exports com.adrijavi;
+    exports com.adrijavi.controlador;
 }
