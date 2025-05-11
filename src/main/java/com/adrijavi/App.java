acccafe7bfbf3c47a4a0c62fceb876efe31fc5df
Javier Villar <javi.villar.jimenez@gmail.com>
2025-05-11 19:50:29 +0200

    creacion de personajes y controladores

--- src/main/java/com/adrijavi/App.java
+++ src/main/java/com/adrijavi/App.java
@@ -6,33 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar el archivo FXML
+        Parent root = FXMLLoader.load(getClass().getResource("/com/adrijavi/vista/VistaCrearPersonaje.fxml"));
+        
+        Scene escena = new Scene(root, 500, 600);
+        
+        primaryStage.setTitle("Creación de Personaje");
+        primaryStage.setScene(escena);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
-}
+}   
