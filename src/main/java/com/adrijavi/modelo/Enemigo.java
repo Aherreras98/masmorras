357d96d967445b35fd3412224b99666bef06138a
Javier Villar <javi.villar.jimenez@gmail.com>
2025-05-12 21:25:04 +0200

    Celdas y vistas juego

---
+++ src/main/java/com/adrijavi/modelo/Enemigo.java
@@ -0,0 +1,7 @@
+package com.adrijavi.modelo;
+
+public class Enemigo extends Personaje {
+    public Enemigo(String nombre, int salud, int fuerza, int defensa, int velocidad, int percepcion, int fila, int columna) {
+        super(nombre, salud, fuerza, defensa, velocidad, percepcion, fila, columna);
+    }
+}
